077d5389700a34b6fed626cbb7fe60c5bb394b91
jyasukawa <jin.yasukawa123@gmail.com>
2025-01-17 00:44:00 +0900

    apply: ChatComponent.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld/>
+      <!-- <HelloWorld/> -->
+      <ChatComponent/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import ChatComponent from './components/ChatComponent'
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    // HelloWorld,
+    ChatComponent,
   },
 
   data: () => ({
