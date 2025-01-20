c966ad8b78d7205150c8264a53d5c20ef9b42315
jyasukawa <jin.yasukawa123@gmail.com>
2025-01-21 00:02:24 +0900

    update: UserList.vue

--- src/views/UserList.vue
+++ src/views/UserList.vue
@@ -1,17 +1,30 @@
 <template>
-  <div class="home">
-		ユーザー一覧
-  </div>
+  <v-app id="inspire">
+    <v-app-bar extended>
+
+      <v-app-bar-title>ユーザー一覧</v-app-bar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon="mdi-dots-vertical">
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col
+            v-for="n in 24"
+            :key="n"
+            cols="4"
+          >
+            <!-- <v-card height="200"></v-card> -->
+						<router-link :to="{ path: '/about', query: { user_id: n }}">
+							<v-avatar class="mb-4" color="grey-lighten-2" size="80"></v-avatar>
+						</router-link>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
-
-<script>
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
-
-// export default {
-//   name: 'HomeView',
-//   components: {
-//     HelloWorld
-//   }
-// }
-</script>
