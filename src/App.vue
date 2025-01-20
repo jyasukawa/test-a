2a065f65082b7fa683e11eec69a96fb3cf2cbbdc
jyasukawa <jin.yasukawa123@gmail.com>
2025-01-20 22:56:06 +0900

    add: vue router

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <v-app>
-    <v-main>
-      <!-- <HelloWorld/> -->
-      <ChatComponent/>
-    </v-main>
-  </v-app>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
 
-<script>
-// import HelloWorld from './components/HelloWorld.vue'
-import ChatComponent from './components/ChatComponent'
-
-export default {
-  name: 'App',
-
-  components: {
-    // HelloWorld,
-    ChatComponent,
-  },
-
-  data: () => ({
-    //
-  }),
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
-</script>
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
